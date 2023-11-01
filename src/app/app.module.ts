f6b8e04f1f56256b85713b25ba843567c03589c2
MasterfulArtist <it21971@hua.gr>
2023-11-01 18:09:55 +0200

    new login and nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './root/app.component';
 import {BrowserModule} from "@angular/platform-browser";
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-     HomeComponent
+     HomeComponent,
+     LoginComponent,
+     NavbarComponent
   ],
   imports: [
     BrowserModule
