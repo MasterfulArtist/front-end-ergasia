2deab67f5da83e689d8cffd885a55f67e47400d3
thanasisadmin <it21971@hua.gr>
2023-12-18 17:12:14 +0200

    Login first step

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  username:string=''
+  password:string=''
+
+  constructor(private authservice:AuthService){
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  login(){
+
+    this.authservice.signin(this.username,this.password);
+
+  }
+
 
 }
